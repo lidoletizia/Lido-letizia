20edad71d0a75276574ad0ea30431d162fe8d853
lidoletizia <biondofabio193@gmail.com>
2025-12-12 18:42:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-import os
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route("/")
 def home():
-    return render_template("home.html", page="home")
-
-@app.route("/stabilimento-balneare")
-def stabilimento():
-    return render_template("stabilimento.html", page="stabilimento")
-
-@app.route("/ristorante-di-pesce")
-def ristorante():
-    return render_template("ristorante.html", page="ristorante")
-
-@app.route("/lounge-bar")
-def lounge():
-    return render_template("lounge.html", page="lounge")
-
-@app.route("/contatti")
-def contatti():
-    return render_template("contatti.html", page="contatti")
+    return "Ciao dal Lido Letizia su Railway!"
 
 if name == "main":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000)
